8cb7c1ae7e0a7140712f87a55e555e1653bb1279
Karthik Chandrasekar <karthidec1@gmail.com>
2014-01-29 14:32:40 -0700

    Started consuming second set of train data. Significant improvement in efficiency of both classifiers. However efficiency is still lower

--- BookClassifier.py
+++ BookClassifier.py
@@ -1,6 +1,9 @@
+#coding=utf-8
+
 import operator, string
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
+from nltk.classify import DecisionTreeClassifier
 import nltk, os, logging, json, ConfigParser, codecs
 from sklearn.svm import LinearSVC
 from nltk.classify.scikitlearn import SklearnClassifier
@@ -9,7 +12,10 @@ from sklearn.metrics import classification_report
 from nltk.metrics import BigramAssocMeasures
 from nltk.collocations import BigramCollocationFinder
 from nltk.corpus import stopwords
+from sklearn.naive_bayes import BernoulliNB
 import nltk;
+import re
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 
 class BookClassifier:
 
@@ -24,6 +30,8 @@ class BookClassifier:
         self.data_dir = os.path.join(cur_dir, rel_dir_path)
         self.train_file_name = self.config.get('GLOBAL', 'train_file_name')
         self.train_file = os.path.join(self.data_dir, self.train_file_name)
+        self.second_train_file_name = self.config.get('GLOBAL', 'second_train_file_name')
+        self.second_train_file = os.path.join(self.data_dir, self.second_train_file_name)        
 
         self.logger_file = os.path.join("OUTPUT", "BookClassifier.log") 
         self.output_file = os.path.join("OUTPUT", "output.txt")
@@ -64,12 +72,15 @@ class BookClassifier:
                     new_temp = []
                     new_temp.extend(self.clean_book_title(temp[2]))
                     new_temp.extend(self.clean_book_author(temp[3]))
+                    new_temp.extend(self.bookid_features_dict.get(temp[1], []))
+                    toc_set = set(self.bookid_features_dict.get(temp[1]))
                     temp_list = []
                     selected_temp = []
                     for feat in new_temp:
-                        if feat and feat in self.best:
+                        if feat and feat in self.best and feat:
                             temp_list.append((feat, True))
-                            selected_temp.append(feat)        
+                            if feat in toc_set:
+                                selected_temp.append(feat)        
                     #temp_list.extend(self.get_bigram(selected_temp))
                     self.selected_feats.append((dict(temp_list), key))            
                 if temp and len(temp) == 3:
@@ -77,7 +88,7 @@ class BookClassifier:
 
     def get_bigram(self, features_list):
         score = BigramAssocMeasures.chi_sq
-        n = 250
+        n = 10
         all_bigrams = BigramCollocationFinder.from_words(features_list)
         best_bigrams = all_bigrams.nbest(score, n)
         selected_bigrams = [(bigram, True) for bigram in best_bigrams]
@@ -116,6 +127,10 @@ class BookClassifier:
         fold_size = int(train_feats_count / 10)
         acc_list = []
         svm_acc_list = []
+        bnb_acc_list = []
+        dtc_acc_list = []
+        rf_acc_list = []
+
         for a in range(10):
             start_index = a * fold_size
             end_index = start_index + fold_size
@@ -136,8 +151,29 @@ class BookClassifier:
             
             svm_acc_list.append(svm_acc)
 
+            #self.bnb_classifier = SklearnClassifier(BernoulliNB()).train(train_features)
+            #bnb_acc = nltk.classify.util.accuracy(self.bnb_classifier, test_features) 
+            #bnb_acc_list.append(bnb_acc)
+            #print "\n ACCURACY - BNB  CLASSIFIER: %s \n" % bnb_acc
+            
+
+            #self.dtc = DecisionTreeClassifier.train(train_features)
+            #dtc_acc = nltk.classify.util.accuracy(self.dtc, test_features)
+            #print "\n ACCURACY - DTC  CLASSIFIER: %s \n" % dtc_acc
+            #dtc_acc_list.append(dtc_acc)        
+    
+
+            self.rf = RandomForestClassifier.train(train_features)            
+            rf_acc = nltk.classify.util.accuracy(self.rf, test_features)
+            print "\n ACCURACY - DTC  CLASSIFIER: %s \n" % rf_acc
+            rf_acc_list.append(dtc_acc)        
+
+
         print 'Average acc %s' % (float(sum(acc_list)/len(acc_list)))
         print 'Average svm acc %s' % (float(sum(svm_acc_list)/len(svm_acc_list)))
+        print 'Bnb acc %s' % (float(sum(bnb_acc_list)/len(bnb_acc_list)))
+        print 'Dtc acc %s' % (float(sum(dtc_acc_list)/len(dtc_acc_list)))
+        print 'Rf acc %s' % (float(sum(rf_acc_list)/len(rf_acc_list)))
 
     def preprocessing(self):
         self.initialize_logger()
@@ -148,7 +184,13 @@ class BookClassifier:
 
 
     def compute_word_scores(self):
-       
+      
+        self.second_train_data()
+        self.train_data()
+
+    
+    def train_data(self):
+ 
         freq_dist_obj = FreqDist()
         cond_freq_dist_obj = ConditionalFreqDist()
         key_set = set() 
@@ -160,7 +202,9 @@ class BookClassifier:
             if not len(temp) == 4:continue;
             key_set.add(temp[0])
             key  = temp[0]
-            for feat in self.clean_book_title(temp[2]):
+            features = self.clean_book_title(temp[2])
+            features.extend(self.bookid_features_dict.get(temp[1], []))
+            for feat in features:
                 freq_dist_obj.inc(feat)
                 cond_freq_dist_obj[key].inc(feat)
             
@@ -177,19 +221,48 @@ class BookClassifier:
         
         self.best =  sorted(word_score_dict.iteritems(), key=operator.itemgetter(1), reverse=True)
 
-        total_select_count = int(len(self.best) * 0.85)
+        #total_select_count = int(len(self.best) * 0.85)
+        total_select_count = 2000
         self.best = self.best[:total_select_count]
         self.best = [pair[0] for pair in self.best]
 
 
+    def clean_book_toc(self, toc):
+        return [word  for word in re.sub("[^a-zA-Z]"," ", toc).split(" ") if word]
+
+    def second_train_data(self):
+        
+        freq_dist_obj = FreqDist()
+        cond_freq_dist_obj = ConditionalFreqDist()
+        self.bookid_features_dict = {}              
+
+        for instance in self.s_instance_list:
+            temp = instance and instance.strip().replace("↵","")
+            if not temp:continue
+            key = temp.split("\t")[0]
+            temp = self.clean_book_toc(temp)
+            self.bookid_features_dict.setdefault(key, []).extend(temp[1:])
+            
     def open_files(self):
         self.train_file_fd = open(self.train_file, 'r') 
+        self.second_train_file_fd = open(self.second_train_file, 'r')
 
     def load_data(self):
+        self.load_train_data()
+        self.load_second_train_data()
+
+
+    def load_train_data(self):
         self.instance_list = []
         for instance in self.train_file_fd.readlines():
             self.instance_list.append(instance) 
         self.instance_list = self.instance_list[1:]
+
+    def load_second_train_data(self):
+        self.s_instance_list = []
+        for instance in self.second_train_file_fd.readlines():
+            self.s_instance_list.append(instance)
+        self.s_instance_list = self.s_instance_list[1:]
 
     def close_files(self):
         self.train_file_fd.close() 
